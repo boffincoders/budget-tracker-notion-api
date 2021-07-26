88124d8bedcc1c12f325ce7a48287df5ba9b4b21
Shri Krishna <manojsethi@live.in>
2021-07-26 09:59:33 +0530

    Sticky List

--- lib/src/screens/budget_screen.dart
+++ lib/src/screens/budget_screen.dart
@@ -7,6 +7,7 @@ import 'package:budget_tracker_notion/src/widgets/spending_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
+import 'package:sticky_grouped_list/sticky_grouped_list.dart';
 
 import 'create_page.dart';
 
@@ -65,71 +66,100 @@ class _BudgetScreenState extends State<BudgetScreen> {
             );
           } else {
             List<ItemModel> items = snapshot.data!;
-            return Refresh(
-              child: ListView.builder(
-                itemCount: items.length + 1,
-                itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return SpendingChart(items: items);
-                  }
+            List<Element> _elements = [];
+            items.forEach((element) {
+              _elements.add(Element(element.date, element));
+            });
 
-                  final item = items[index - 1];
-                  return Slidable(
-                    controller: slidableController,
-                    key: Key(item.pageId),
-                    actionPane: _getActionPane(index),
-                    actionExtentRatio: 0.25,
-                    child: Container(
-                      margin: const EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10.0),
-                        border: Border.all(
-                          width: 2.0,
-                          color: getCategoryColor(item.category),
+            return Container(
+              child: Column(
+                children: [
+                  SpendingChart(items: items),
+                  Expanded(
+                    child: Refresh(
+                      child: StickyGroupedListView<Element, DateTime>(
+                        elements: _elements,
+                        order: StickyGroupedListOrder.DESC,
+                        groupBy: (Element element) => DateTime(
+                          element.date.year,
+                          element.date.month,
                         ),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.black26,
-                            offset: Offset(0, 2),
-                            blurRadius: 6.0,
-                          ),
-                        ],
-                      ),
-                      child: ListTile(
-                        title: Text(item.name),
-                        subtitle: Text(
-                            '${item.category} - ${DateFormat.yMd().format(item.date)}'),
-                        trailing: Text(
-                          '-\$${item.price.toStringAsFixed(2)}',
-                        ),
-                      ),
-                    ),
-                    secondaryActions: <Widget>[
-                      /* IconSlideAction(
-                        caption: 'Edit',
-                        color: Colors.black45,
-                        icon: Icons.edit,
-                        onTap: () {},
-                      ),*/
-                      IconSlideAction(
-                        caption: 'Delete',
-                        color: Colors.red[400],
-                        icon: Icons.delete,
-                        onTap: () async {
-                          var response = await bloc.deletePage(item.pageId);
-                          final int statusCode = response.statusCode;
-                          if (statusCode == 200) {
-                            bloc.fetchItems();
-                          } else {
-                            final data = jsonDecode(response.body);
-                            throw Failure(message: data['message']);
-                          }
+                        groupSeparatorBuilder: (Element element) => Container(
+                            width: MediaQuery.of(context).size.width,
+                            color: Color(0x45C4C4C4),
+                            child: Padding(
+                              padding:
+                                  EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+                              child: Text(
+                                '${getMonth(element.date.month)} ${element.date.year}',
+                                style: TextStyle(
+                                    fontSize: 12.0, color: Color(0xff000000)),
+                              ),
+                            )),
+                        floatingHeader: false,
+                        itemBuilder: (_, Element element) {
+                          return Slidable(
+                            controller: slidableController,
+                            key: Key(element.itemData.pageId),
+                            actionPane: SlidableStrechActionPane(),
+                            actionExtentRatio: 0.25,
+                            child: Container(
+                              margin: const EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10.0),
+                                border: Border.all(
+                                  width: 2.0,
+                                  color: getCategoryColor(
+                                      element.itemData.category),
+                                ),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black26,
+                                    offset: Offset(0, 2),
+                                    blurRadius: 6.0,
+                                  ),
+                                ],
+                              ),
+                              child: ListTile(
+                                title: Text(element.itemData.name),
+                                subtitle: Text(
+                                    '${element.itemData.category} - ${DateFormat.yMd().format(element.date)}'),
+                                trailing: Text(
+                                  '-\$${element.itemData.price.toStringAsFixed(2)}',
+                                ),
+                              ),
+                            ),
+                            secondaryActions: <Widget>[
+                              /* IconSlideAction(
+                                caption: 'Edit',
+                                color: Colors.black45,
+                                icon: Icons.edit,
+                                onTap: () {},
+                              ),*/
+                              IconSlideAction(
+                                caption: 'Delete',
+                                color: Colors.red[400],
+                                icon: Icons.delete,
+                                onTap: () async {
+                                  var response = await bloc
+                                      .deletePage(element.itemData.pageId);
+                                  final int statusCode = response.statusCode;
+                                  if (statusCode == 200) {
+                                    bloc.fetchItems();
+                                  } else {
+                                    final data = jsonDecode(response.body);
+                                    throw Failure(message: data['message']);
+                                  }
+                                },
+                              ),
+                            ],
+                          );
                         },
                       ),
-                    ],
-                  );
-                },
+                    ),
+                  ),
+                ],
               ),
             );
           }
@@ -165,4 +195,34 @@ Widget _getActionPane(int index) {
     default:
       return SlidableBehindActionPane();
   }
+}
+
+class Element implements Comparable {
+  DateTime date;
+  ItemModel itemData;
+
+  Element(this.date, this.itemData);
+
+  @override
+  int compareTo(other) {
+    return date.compareTo(other.date);
+  }
+}
+
+String getMonth(int index) {
+  const monthNames = [
+    "JANUARY",
+    "FEBRUARY",
+    "MARCH",
+    "APRIL",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUGUST",
+    "SEPTEMBER",
+    "OCTOBER",
+    "NOVEMBER",
+    "DECEMBER"
+  ];
+  return monthNames[index - 1];
 }
